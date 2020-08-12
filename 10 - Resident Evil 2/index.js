23fc79796d1d03b1c14d58fbac8ab9287cdb63a4
oGuimaraes <otaviovgsr@gmail.com>
2020-08-11 21:40:53 -0300

    esboço - task 10

---
+++ 10 - Resident Evil 2/index.js
@@ -0,0 +1,22 @@
+let arr = [];
+let actualNumber = 1;
+let itemsInLine = 1;
+let line = 0;
+const anoInauguracaoEstacao = 1969;
+
+while (actualNumber < anoInauguracaoEstacao) {
+  arr = new Array();
+  line++;
+  for (j = 1; j <= itemsInLine; j++) {
+    arr.push(actualNumber);
+    actualNumber++;
+    if (actualNumber > anoInauguracaoEstacao) {
+      break;
+    }
+  }
+  itemsInLine += 2;
+  console.log(`[${line}] -> ${arr}`);
+}
+console.log(
+  `O número ${anoInauguracaoEstacao} foi encontrado na linha ${line}`
+);
