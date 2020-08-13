63101c3fb77c862d56f3d94ba4b168aaf97b4ff5
oGuimaraes <otaviovgsr@gmail.com>
2020-08-13 11:34:32 -0300

    Challenge 6, 3 letras done

--- 6 - Tomb Raider/index.js
+++ 6 - Tomb Raider/index.js
@@ -1,1 +1,23 @@
-let senha = "";
+const letras = ["A", "B", "C", "D", "E", "F"];
+const possiveisSenhas = [];
+
+for (i = 0; i < letras.length; i++) {
+  let senha = "";
+  senha = letras[i];
+  //console.log(senha);
+  for (j = 0; j < letras.length; j++) {
+    //console.log(`${senha}${letras[j]}`);
+    //possiveisSenhas.push((senha += letras[j]));
+    senha += letras[j];
+    //console.log(senha);
+    for (k = 0; k < letras.length; k++) {
+      possiveisSenhas.push((senha += letras[k]));
+      senha = `${letras[i]}${letras[j]}`;
+    }
+    senha = letras[i];
+  }
+}
+for (a = 0; a < possiveisSenhas.length; a++) {
+  console.log(possiveisSenhas[a]);
+}
+console.log(possiveisSenhas.length);
