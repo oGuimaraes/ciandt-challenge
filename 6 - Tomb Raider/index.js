cb591e3444aef7ac60ffe935951883246623f28d
oGuimaraes <otaviovgsr@gmail.com>
2020-08-13 11:41:31 -0300

    Challenge 6, 4 char done

--- 6 - Tomb Raider/index.js
+++ 6 - Tomb Raider/index.js
@@ -4,20 +4,22 @@ const possiveisSenhas = [];
 for (i = 0; i < letras.length; i++) {
   let senha = "";
   senha = letras[i];
-  //console.log(senha);
   for (j = 0; j < letras.length; j++) {
-    //console.log(`${senha}${letras[j]}`);
-    //possiveisSenhas.push((senha += letras[j]));
     senha += letras[j];
-    //console.log(senha);
     for (k = 0; k < letras.length; k++) {
-      possiveisSenhas.push((senha += letras[k]));
+      senha += letras[k];
+      for (l = 1; l <= 9; l++) {
+        possiveisSenhas.push((senha += l));
+        senha = `${letras[i]}${letras[j]}${letras[k]}`;
+      }
       senha = `${letras[i]}${letras[j]}`;
     }
-    senha = letras[i];
+    senha = `${letras[i]}`;
   }
 }
-for (a = 0; a < possiveisSenhas.length; a++) {
+/*for (a = 0; a < possiveisSenhas.length; a++) {
   console.log(possiveisSenhas[a]);
 }
+*/
+console.log(possiveisSenhas);
 console.log(possiveisSenhas.length);
